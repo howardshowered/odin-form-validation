9eee7352f21db9a88cccb32bcff9f2a3a57b9b48
Howard Chen <194877133+howardshowered@users.noreply.github.com>
2025-12-20 11:18:42 -0800

    fixed some styling on the fields and blur/input events.

--- script.js
+++ script.js
@@ -3,13 +3,9 @@ const postalInput = document.querySelector("#post-code");
 const passwordInput = document.querySelector("#password");
 const passwordConfirmInput = document.querySelector("#password-confirm");
 
+
 emailInput.addEventListener('blur', () => {
-    
-    if (emailInput.validity.typeMismatch) {
-        emailInput.style.backgroundColor = "green";
-        emailInput.setCustomValidity("Not of email type.");
-        emailInput.reportValidity();
-    } 
+
     if (emailInput.validity.valueMissing) {
         console.log("value missing");
         emailInput.style.backgroundColor = "red";
@@ -17,9 +13,26 @@ emailInput.addEventListener('blur', () => {
         emailInput.reportValidity();
     }
 });
+emailInput.addEventListener('input', () => {
 
+    if (emailInput.validity.typeMismatch) {
+        emailInput.style.backgroundColor = "red";
+        emailInput.setCustomValidity("Not of email type.");
+        emailInput.reportValidity();
+    } 
+
+});
 
 postalInput.addEventListener('blur', () => {
+    if(postalInput.validity.valueMissing) {
+        postalInput.style.backgroundColor = "red";
+        postalInput.setCustomValidity("No postal code entered");
+        postalInput.reportValidity();
+    }
+});
+
+
+postalInput.addEventListener('input', () => {
     if(postalInput.validity.valueMissing) {
         postalInput.style.backgroundColor = "red";
         postalInput.setCustomValidity("No postal code entered");
@@ -33,4 +46,47 @@ postalInput.addEventListener('blur', () => {
     }
     
 
+});
+
+
+passwordInput.addEventListener('blur', () => {
+
+    if (passwordInput.validity.valueMissing) {
+        console.log("value missing");
+        passwordInput.style.backgroundColor = "red";
+        passwordInput.setCustomValidity("No password entered");
+        passwordInput.reportValidity();
+    }
+});
+
+passwordInput.addEventListener('input', () => {
+    if(postalInput.validity.patternMismatch ) {
+        postalInput.style.backgroundColor = "red";
+        postalInput.setCustomValidity("Password not in correct format");
+        postalInput.reportValidity();
+    }
+});
+passwordConfirmInput.addEventListener('blur', () => {
+    
+    if (passwordConfirmInput.validity.valueMissing) {
+        console.log("value missing");
+        passwordConfirmInput.style.backgroundColor = "red";
+        passwordConfirmInput.setCustomValidity("No password entered");
+        passwordConfirmInput.reportValidity();
+    }
+});
+
+passwordConfirmInput.addEventListener('input', () => {
+    
+    if(passwordConfirmInput.validity.patternMismatch ) {
+        passwordConfirmInput.style.backgroundColor = "red";
+        passwordConfirmInput.setCustomValidity("Password not in correct format");
+        passwordConfirmInput.reportValidity();
+    }
+
+    if(passwordConfirmInput.value !== passwordInput.value ) {
+        passwordConfirmInput.style.backgroundColor = "red";
+        passwordConfirmInput.setCustomValidity("Password confirmation not the same.");
+        passwordConfirmInput.reportValidity();
+    }
 });
