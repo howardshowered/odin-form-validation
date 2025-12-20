411524a057da2c8f66343f7787e0499087d34a02
Howard Chen <194877133+howardshowered@users.noreply.github.com>
2025-12-19 21:55:08 -0800

    added validation for bothe email and postal code fields

--- script.js
+++ script.js
@@ -1,13 +1,36 @@
 const emailInput = document.querySelector("#email");
-const postalInput = document.querySelector("#postal-code");
+const postalInput = document.querySelector("#post-code");
 const passwordInput = document.querySelector("#password");
 const passwordConfirmInput = document.querySelector("#password-confirm");
 
 emailInput.addEventListener('blur', () => {
-    console.log("blured");
+    
     if (emailInput.validity.typeMismatch) {
-        emailInput.style.backgroundColor = "red";
+        emailInput.style.backgroundColor = "green";
         emailInput.setCustomValidity("Not of email type.");
         emailInput.reportValidity();
+    } 
+    if (emailInput.validity.valueMissing) {
+        console.log("value missing");
+        emailInput.style.backgroundColor = "red";
+        emailInput.setCustomValidity("No email entered");
+        emailInput.reportValidity();
     }
+});
+
+
+postalInput.addEventListener('blur', () => {
+    if(postalInput.validity.valueMissing) {
+        postalInput.style.backgroundColor = "red";
+        postalInput.setCustomValidity("No postal code entered");
+        postalInput.reportValidity();
+    }
+
+    if(postalInput.validity.patternMismatch ) {
+        postalInput.style.backgroundColor = "red";
+        postalInput.setCustomValidity("No postal code not correct");
+        postalInput.reportValidity();
+    }
+    
+
 });
